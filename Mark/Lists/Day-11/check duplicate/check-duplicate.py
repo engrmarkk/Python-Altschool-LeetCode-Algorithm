990e25eff9a8362dcba5868371f796f233bae9ed
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-01-08 23:53:43 +0000

    modifed

--- Mark/Lists/Day-11/check duplicate/check-duplicate.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
-        return False if len(nums)==len(set(nums)) else True
-
-print(Solution().containsDuplicate([3,4,5]))
